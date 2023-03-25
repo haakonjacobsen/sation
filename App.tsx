8c34a76508590b392c974345615299b49d4cadd1
haakonjacobsen <haakonjacobsen95@gmail.com>
2023-03-25 20:26:19 +0100

    Add react query

--- App.tsx
+++ App.tsx
@@ -9,72 +9,76 @@ import {RootStackParamList} from "./types/navigation";
 import AudioScreen from "./screens/AudioScreen";
 import RecordScreen from "./screens/RecordScreen";
 import { RecoilRoot} from 'recoil';
+import {QueryClient, QueryClientProvider} from "react-query";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const queryClient = new QueryClient();
 
 export default function App() {
 
   return (
     <RecoilRoot>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={({ navigation }) => {
-          return {
-            contentStyle: {
-              backgroundColor: theme.background.primary,
-            },
-            animation: 'slide_from_left',
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => navigation.goBack()}>
-                <MaterialIcons name="keyboard-arrow-left" size={30} color={theme.text.primary} />
-              </TouchableOpacity>
-            )
-          };
-        }}>
-          <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{
-              headerShown: false,
-              headerStyle: {
-                backgroundColor: theme.background.secondary,
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={({ navigation }) => {
+            return {
+              contentStyle: {
+                backgroundColor: theme.background.primary,
               },
-              headerTransparent: false,
-              headerTitle: 'Home',
-              headerTitleStyle: {
-                color: theme.text.primary,
-                fontWeight: '700'
-              }
-          }}/>
-          <Stack.Screen
-            name="AudioScreen"
-            component={AudioScreen}
-            options={{
-              headerStyle: {
-                backgroundColor: theme.background.secondary,
-              },
-              headerTransparent: false,
-              headerTitle: 'Audio',
-              headerTitleStyle: {
-                color: theme.text.primary,
-                fontWeight: '700'
-              }
+              animation: 'slide_from_left',
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                  <MaterialIcons name="keyboard-arrow-left" size={30} color={theme.text.primary} />
+                </TouchableOpacity>
+              )
+            };
+          }}>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+                headerStyle: {
+                  backgroundColor: theme.background.secondary,
+                },
+                headerTransparent: false,
+                headerTitle: 'Home',
+                headerTitleStyle: {
+                  color: theme.text.primary,
+                  fontWeight: '700'
+                }
             }}/>
-          <Stack.Screen
-            name="RecordScreen"
-            component={RecordScreen}
-            options={{
-              headerStyle: {
-                backgroundColor: theme.background.secondary,
-              },
-              headerTransparent: false,
-              headerTitle: 'Audio',
-              headerTitleStyle: {
-                color: theme.text.primary,
-                fontWeight: '700'
-              }
-            }}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="AudioScreen"
+              component={AudioScreen}
+              options={{
+                headerStyle: {
+                  backgroundColor: theme.background.secondary,
+                },
+                headerTransparent: false,
+                headerTitle: 'Audio',
+                headerTitleStyle: {
+                  color: theme.text.primary,
+                  fontWeight: '700'
+                }
+              }}/>
+            <Stack.Screen
+              name="RecordScreen"
+              component={RecordScreen}
+              options={{
+                headerStyle: {
+                  backgroundColor: theme.background.secondary,
+                },
+                headerTransparent: false,
+                headerTitle: 'Audio',
+                headerTitleStyle: {
+                  color: theme.text.primary,
+                  fontWeight: '700'
+                }
+              }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
